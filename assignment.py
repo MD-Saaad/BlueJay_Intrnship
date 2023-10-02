b907d081ea63416813193035c4b0a95d0f2a07a0
mohammed saad sayyed <sayyedsaad1634@gmail.com>
2023-10-02 21:41:09 +0530

    Added the .txt file and updated the code

--- assignment.py
+++ assignment.py
@@ -14,6 +14,9 @@ df.sort_values(['Employee Name', 'Working Date'], inplace=True)
 consecutive_days = 0
 prev_checkout_time = None
 
+# Create a list to store messages
+messages = []
+
 # Iterate through the DataFrame
 for index, row in df.iterrows():
     # Attempt to parse the 'Working Date' column to datetime
@@ -36,13 +39,15 @@ for index, row in df.iterrows():
 
     # Check if the employee has worked for more than 14 hours in a single shift
     if time_diff.total_seconds() / 3600 > 14:
-        print(f"Employee_Name: {row['Employee Name']} \n (Position ID: {row['Position ID']}) \n worked more than 14 hours on {working_date}")
+        message = f"Employee_Name: {row['Employee Name']} \n(Position ID: {row['Position ID']}) \nworked more than 14 hours on {working_date}"
+        messages.append(message)
 
     # Check for consecutive days and less than 10 hours between shifts
     if prev_checkout_time is not None:
         time_between_shifts = checkin_time - prev_checkout_time
-        if time_between_shifts.total_seconds() / 3600 < 10 and time_between_shifts.total_seconds() / 3600 > 1:
-            print(f"Employee_Name: {row['Employee Name']} \n (Position ID: {row['Position ID']}) \n has less than 10 hours between shifts on {working_date}")
+        if 1 < time_between_shifts.total_seconds() / 3600 < 10:
+            message = f"Employee_Name: {row['Employee Name']} \n(Position ID: {row['Position ID']}) \nhas less than 10 hours between shifts on {working_date}"
+            messages.append(message)
 
     # Check if there's a next row and if the current row and the next row have the same employee name
     if index + 1 < len(df) and row['Employee Name'] == df.iloc[index + 1]['Employee Name']:
@@ -51,7 +56,8 @@ for index, row in df.iterrows():
         if (next_working_date - working_date).days == 1:
             consecutive_days += 1
             if consecutive_days > 6:
-                print(f"Employee_Name: {row['Employee Name']} \n (Position ID: {row['Position ID']}) \n has worked for 7 consecutive days from {working_date - timedelta(days=6)} to {next_working_date}")
+                message = f"Employee_Name: {row['Employee Name']} \n(Position ID: {row['Position ID']}) \nhas worked for 7 consecutive days from {working_date - timedelta(days=6)} to {next_working_date}"
+                messages.append(message)
         else:
             consecutive_days = 0
     else:
@@ -59,5 +65,7 @@ for index, row in df.iterrows():
 
     prev_checkout_time = checkout_time
 
-# Save the cleaned DataFrame back to a new CSV file which dont have null values
-df.to_csv('CleanedEmployee.csv', index=False)
+# Save messages to a text file
+with open('employee_messages.txt', 'w') as file:
+    for message in messages:
+        file.write(message + '\n')
